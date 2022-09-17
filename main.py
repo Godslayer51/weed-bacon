30e0540f9d3631af97adefbfead9c08c0a97c807
Godslayer51 <104952008+Godslayer51@users.noreply.github.com>
2022-09-17 14:08:17 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ app = Flask(__name__)
 def payload_receiver():
     print("Reloading project")
     print("Test successful")
-    print("Test again")
     subprocess.Popen("git pull",shell=True)
     subprocess.Popen("sudo ps aux |grep gunicorn |grep projectname | awk '{ print $2 }' |xargs kill -HUP", shell=True)
     return "ok"
