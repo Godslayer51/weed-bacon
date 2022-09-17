4c36cee568f993be329f47bd2e0018ee59d293c8
Godslayer51 <104952008+Godslayer51@users.noreply.github.com>
2022-09-17 14:04:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 @app.route('/payload_receiver', methods=['POST'])
 def payload_receiver():
+    print("Reloading project")
     subprocess.Popen("git pull",shell=True)
     return "ok"
 
